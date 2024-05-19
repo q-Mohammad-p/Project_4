3eb2abf9391844efc3ffc1757bc2c63578b30b8c
tessst <mohhgfd@gmail.com>
2024-05-19 20:18:07 +0330

    creating address.h

---
+++ address.h
@@ -0,0 +1,46 @@
+//
+// Created by mobile kavosh on 5/19/2024.
+//
+
+#ifndef PROJECT_4_2_ADDRESS_H
+#define PROJECT_4_2_ADDRESS_H
+
+#include <iostream>
+
+using namespace std;
+
+class Address {
+private:
+    string country;
+    string city;
+    string street;
+public:
+    Address() : country("unknown"), city("unknown"), street("unknown ") {};
+
+    Address(string &country, string &city, string &street) :
+            country(country), city(city), street(street) {};
+
+    friend ostream &operator<<(ostream &os, const Address &object);
+
+    friend istream &operator>>(istream &is, Address &object);
+
+    void set_country(const string &name) { country = name; };
+
+    void set_city(const string &name) { city = name; };
+
+    void set_street(const string &name) { street = name; };
+
+//    void set_all_address(const string, const string, const string);
+
+    string get_country() const { return country; };
+
+    string get_city() const { return city; };
+
+    string get_street() const { return street; };
+
+
+};
+};
+
+
+#endif //PROJECT_4_2_ADDRESS_H
