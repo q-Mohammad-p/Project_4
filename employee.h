de357c2143636097f2af3ad89cfdeb4c87d3a70e
tessst <mohhgfd@gmail.com>
2024-05-21 00:09:58 +0330

    getter and setter functions

--- employee.h
+++ employee.h
@@ -34,6 +34,34 @@ public:
     friend istream &operator>>(istream &istream1, Employee &e1);
 
     Employee &operator=(const Employee &e1);
+
+    void set_name(const string& Name){name = Name;};
+
+    void set_id(const string& ID){id = ID;};
+
+    void set_address(const Address& ad){address = ad;};
+
+    void set_hourWork(int num){hourWork = num;};
+
+    void set_salaryPerHour(int salary){salaryPerHour = salary;};
+
+    void set_workToDo(int work){workToDo = work;};
+
+    void set_workDone(int done){workDone = done;};
+
+    string get_name() const{return name;};
+
+    string get_id()const {return id;};
+
+    Address get_address()const {return address;};
+
+    int get_hourWork()const {return hourWork;};
+
+    int get_salaryPerHour()const {return salaryPerHour;};
+
+    int get_workToDo()const {return workToDo;};
+
+    int get_workDone()const {return workDone;};
 };
 
 
