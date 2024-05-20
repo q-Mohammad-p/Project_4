2a81a3596e0489f001229b3b380d2d996384473f
tessst <mohhgfd@gmail.com>
2024-05-20 21:10:02 +0330

    edited

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "address.cpp"
+#include "person.cpp"
+#include "employee.cpp"
+
+
+int main() {
+    Address a;
+    Person p1;
+    Employee b;
+    cin>>b;
+    cout<<b;
+//    Person p1("ali","91b03019",a);
+//    p1.validate();
+//    cin>>p1;
+//    cout<<p1;
+    return 0;
+}
